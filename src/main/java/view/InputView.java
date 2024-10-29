b753e93fae15e4f75f42e0dc093f25d78eae3c83
hyebin <shin378378@naver.com>
2024-10-30 06:13:29 +0900

    feat : inputView 생성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,16 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
+    Scanner scanner = new Scanner(System.in);
+    public int inputLadderWidth(){
+        System.out.println("사다리의 넓이는 몇 개인가요?");
+        return scanner.nextInt();
+    }
+
+    public int inputLadderHeight(){
+        System.out.println("사다리의 높이는 몇 개인가요?");
+        return scanner.nextInt();
+    }
 }
